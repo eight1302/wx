17a2ee8d9056528a3a92e09535bc951514052c19
Eight <380080496@qq.com>
2019-02-11 15:19:49 +0800

    Add files via upload

---
+++ wxkf/pages/home/home.js
@@ -0,0 +1,15 @@
+Page({
+  data: {
+    imgUrls: [
+      '../img/timg.jpg',
+      '../img/timg1.jpg',
+      '../img/timg2.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000,
+    bg: '#C79C77',
+    Height:"0"     //这是swiper要动态设置的高度属性
+  }
+})
